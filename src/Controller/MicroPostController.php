266712ef5374b04e21b661b5e992e0612733153c
Houcine <houcinehannoun@gmail.com>
2025-10-12 23:03:40 +0200

    discover doctrine docker

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\MicroPost;
+use App\Repository\MicroPostRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +12,18 @@ use Symfony\Component\Routing\Attribute\Route;
 final class MicroPostController extends AbstractController
 {
     #[Route('/micro-post', name: 'app_micro_post')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager, MicroPostRepository $postsRepository): Response
     {
-        return $this->render('micro_post/index.html.twig', [
-            'controller_name' => 'MicroPostController',
-        ]);
+        return $this->render('micro_post/index.html.twig',
+            ['posts' => $postsRepository->findAll(),]
+        );
+    }
+
+    #[Route('/micro-post/{post}', name: 'app_micro_post_show')]
+    public function showOne(MicroPost $post): Response
+    {
+        return $this->render('micro_post/show_post.html.twig',
+            ['post' => $post]
+        );
     }
 }
