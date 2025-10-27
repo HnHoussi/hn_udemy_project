f3f77826139893e7fead0d4db550b4a11b482403
HnHoussi <houcinehannoun@gmail.com>
2025-10-27 17:15:10 +0100

    like and follow features

---
+++ src/Controller/FollowerController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Exception\ORMException;
+use Doctrine\ORM\OptimisticLockException;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class FollowerController extends AbstractController
+{
+    /**
+     * @throws OptimisticLockException
+     * @throws ORMException
+     */
+    #[Route('/follow/{id}', name: 'app_follow')]
+    public function follow(User $userToFollow, Request $request, EntityManager $entityManager): Response
+    {
+        /** @var User $currentUser */
+        $currentUser = $this->getUser();
+
+        if ($userToFollow->getId() !== $currentUser->getId()) {
+            $currentUser->follow($userToFollow);
+            $entityManager->flush();
+
+        }
+            return $this->redirect($request->headers->get('referer'));
+
+    }
+
+    /**
+     * @throws OptimisticLockException
+     * @throws ORMException
+     */
+    #[Route('/unfollow/{id}', name: 'app_unfollow')]
+    public function unfollow(User $userToUnfollow, Request $request, EntityManager $entityManager): Response
+    {
+        /** @var User $currentUser */
+        $currentUser = $this->getUser();
+
+        if ($userToUnfollow->getId() !== $currentUser->getId()) {
+            $currentUser->unfollow($userToUnfollow);
+            $entityManager->flush();
+        }
+
+        return $this->redirect($request->headers->get('referer'));
+
+    }
+}
---
+++ src/Controller/LikeController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\MicroPost;
+use App\Repository\MicroPostRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Exception\ORMException;
+use Doctrine\ORM\OptimisticLockException;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+final class LikeController extends AbstractController
+{
+    /**
+     * @throws OptimisticLockException
+     * @throws ORMException
+     */
+    #[Route('/like/{id}', name: 'app_like')]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    public function like(
+        MicroPost $post,
+        Request $request,
+        EntityManager $entityManager
+    ): Response {
+        $currentUser = $this->getUser();
+        $post->addLikedBy($currentUser);
+        $entityManager->persist($post);
+        $entityManager->flush();
+
+        return $this->redirect($request->headers->get('referer'));
+    }
+
+    /**
+     * @throws OptimisticLockException
+     * @throws ORMException
+     */
+    #[Route('/unlike/{id}', name: 'app_unlike')]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    public function unlike(
+        MicroPost $post,
+        Request $request,
+        EntityManager $entityManager
+    ): Response {
+        $currentUser = $this->getUser();
+        $post->removeLikedBy($currentUser);
+        $entityManager->persist($post);
+        $entityManager->flush();
+
+        return $this->redirect($request->headers->get('referer'));
+    }
+}
